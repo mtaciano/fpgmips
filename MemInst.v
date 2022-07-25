b83bcf8952ef58e3fb8069882a16b8620f443f02
Miguel Taciano <40744030+mtaciano@users.noreply.github.com>
2022-07-25 19:35:58 -0300

    Uso de mem em arquivo

--- MemInst.v
+++ MemInst.v
@@ -14,15 +14,7 @@ reg [31:0] memoriaI [31:0];
 
 always @(posedge clock) begin
     if (~inicializado) begin
-
-        memoriaI[0] = 32'b10010_00001_0000000000000000000101; // LOADI 5 NO REG1
-          memoriaI[1] = 32'b01110_00001_00001_00000000000001010; // SUBI REG4 - 10 (= -5)
-          memoriaI[2] = 32'b01100_00001_00001_00000000000001010; // SUBI REG4 + 10 (= 5)
-          memoriaI[3] = 32'b00001_000000000000000000000000000; // HLT
-
-          // $readmemb("arquivo.txt", variavel);
-
-          // teste
+        $readmemb("out_bin.txt", memoriaI);
 
         inicializado <= 1;
     end
